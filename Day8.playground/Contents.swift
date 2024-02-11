54170ff6209821f2a4750472c1beb6330e4fde83
Jay Ramirez <ramirezblancasjuanm@gmail.com>
2024-02-11 01:14:40 -0600

    Day 9

--- Day8.playground/Contents.swift
+++ Day8.playground/Contents.swift
@@ -48,7 +48,7 @@ progress.amount = 100
 //Methods
 struct City {
     var population: Int
-
+    
     func collectTaxes() -> Int {
         return population * 1000
     }
@@ -60,7 +60,7 @@ london.collectTaxes()
 //Mutating methods
 struct Person {
     var name: String
-
+    
     mutating func makeAnonymous() {
         name = "Anonymous"
     }
@@ -82,3 +82,83 @@ toys.append("Buzz")
 toys.firstIndex(of: "Buzz")
 print(toys.sorted())
 toys.remove(at: 0)
+
+
+/**
+ Day 9: Structs, part two
+ */
+//Initializers
+struct User {
+    var username: String
+    init() {
+        username = "Anonymous"
+        print("Creating a new user!")
+    }
+}
+
+var user = User()
+print(user.username)
+user.username = "twostraws"
+print(user.username)
+
+//Referring to the current instance
+struct PersonInstance {
+    var name: String
+    
+    init(name: String) {
+        print("\(name) was born!")
+        self.name = name
+    }
+}
+
+var persn = PersonInstance(name: "Jay")
+
+//Lazy properties
+struct FamilyTree {
+    init() {
+        print("Creating family tree!")
+    }
+}
+struct LazyPerson {
+    var name: String
+    lazy var familyTree = FamilyTree() //This creates a lazy instance, only wen needed.
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+var ed = LazyPerson(name: "Ed")
+ed.familyTree
+
+//Static properties and methods
+struct Student {
+    static var classSize = 0
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+        Student.classSize += 1
+    }
+}
+print(Student.classSize)
+var student1 = Student(name: "Jay")
+var student2 = Student(name: "Carlos")
+print(Student.classSize)
+
+//Access control
+struct PersonControl {
+    private var id: String
+    
+    init(id: String) {
+        self.id = id
+    }
+    
+    func identify() -> String {
+        return "My social security number is \(id)"
+    }
+}
+let userControl = PersonControl(id: "12345")
+
+
+//Structs summary
